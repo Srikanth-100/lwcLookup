a18cf36901575bb8118a21a1dd6bb6b5221101da
srikanth <kolanu.srikanth7@gmail.com>
2020-08-14 13:01:46 +0530

    Initial Commit

---
+++ force-app/main/default/lwc/lwcLookupDemo/lwcLookupDemo.js
@@ -0,0 +1,10 @@
+import {LightningElement} from 'lwc';
+
+export default class CustomLookupExample extends LightningElement {
+    handleAccountSelection(event){
+        console.log("the selected record id is"+event.detail);
+    }
+    handleContactSelection(event){
+        console.log("the selected record id is"+event.detail);
+    }
+}
